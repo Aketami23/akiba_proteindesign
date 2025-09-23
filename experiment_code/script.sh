0c6a046f34097f140a7af1d88cc05975fe0a9c15
Aketami23 <136186146+Aketami23@users.noreply.github.com>
2025-09-24 02:52:18 +0900

    config.yamlの実験設定を更新し、script.shから結果CSVのヘッダー出力を削除

--- experiment_code/script.sh
+++ experiment_code/script.sh
@@ -2,7 +2,6 @@ DUMMY_INPUT="./data/dummy.fasta"
 mkdir output
 OUTPUT_DIR="output"
 RESULT_CSV="output/results.csv"
-echo "id","parent_ids","generation","negative_tm_score","recovery","negative_plddt_score","raw_jobname","query_sequence", > "$RESULT_CSV"
 YAML_CONFIG="config.yaml"
 python3.10 main.py\
     "$DUMMY_INPUT" \
