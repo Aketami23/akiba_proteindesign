a380d5f7efa4ffff4de14cfb92a9da4486a77492
Aketami23 <136186146+Aketami23@users.noreply.github.com>
2025-09-23 22:13:35 +0900

    install_dependencies.sh: improve installation script structure and add numpy dependency

--- experiment_code/install_dependencies.sh
+++ experiment_code/install_dependencies.sh
@@ -1,50 +1,59 @@
 #!/bin/bash
-set -eux 
+set -eux
 
-source .venv/bin/activate
+if [ -d ".venv" ]; then
+  source .venv/bin/activate
+fi
 
 pip install --upgrade pip
-pip install Cython setuptools
+pip install Cython setuptools numpy
 
-# 1. Doxygen
+# ===== 1. Doxygen =====
 DOXYGEN_VER=1.9.8
 if [ ! -d "doxygen-${DOXYGEN_VER}" ]; then
   wget -q https://www.doxygen.nl/files/doxygen-${DOXYGEN_VER}.linux.bin.tar.gz
   tar -xzf doxygen-${DOXYGEN_VER}.linux.bin.tar.gz
 fi
-export PATH=$PWD/doxygen-${DOXYGEN_VER}/bin:$PATH
+export PATH="$PWD/doxygen-${DOXYGEN_VER}/bin:$PATH"
 
-# 2. SWIG
+# ===== 2. SWIG =====
 SWIG_VER=4.2.1
-if [ ! -x "$HOME/apps/swig/${SWIG_VER}/bin/swig" ]; then
+SWIG_PREFIX="${HOME}/.local/swig/${SWIG_VER}"
+if [ ! -x "${SWIG_PREFIX}/bin/swig" ]; then
   wget -q https://github.com/swig/swig/archive/refs/tags/v${SWIG_VER}.tar.gz
   tar -xzf v${SWIG_VER}.tar.gz
-  cd swig-${SWIG_VER}
+  pushd swig-${SWIG_VER}
   ./autogen.sh
-  ./configure --prefix=$HOME/apps/swig/${SWIG_VER}
-  make -j$(nproc) && make install
-  cd ..
+  ./configure --prefix="${SWIG_PREFIX}"
+  make -j"$(nproc)" && make install
+  popd
 fi
-export PATH=$HOME/apps/swig/${SWIG_VER}/bin:$PATH
+export PATH="${SWIG_PREFIX}/bin:$PATH"
 
-# 3. OpenMM
+# ===== 3. OpenMM =====
 OPENMM_VER=8.1.1
 if [ ! -d "openmm-${OPENMM_VER}" ]; then
-  wget -q https://github.com/openmm/openmm/archive/refs/tags/${OPENMM_VER}.tar.gz
-  tar -xzf ${OPENMM_VER}.tar.gz
+  wget -q https://github.com/openmm/openmm/archive/refs/tags/${OPENMM_VER}.tar.gz -O openmm-${OPENMM_VER}.tar.gz
+  tar -xzf openmm-${OPENMM_VER}.tar.gz
 fi
 
-cd openmm-${OPENMM_VER}
+SITE_PACKAGES=$(python -c 'import site; print(site.getsitepackages()[0])')
+
+pushd openmm-${OPENMM_VER}
 rm -rf build
 mkdir build && cd build
 
 cmake .. \
-  -DCMAKE_INSTALL_PREFIX=$HOME/apps/openmm/${OPENMM_VER} \
-  -DPYTHON_EXECUTABLE=$(which python) \
-  -DDOXYGEN_EXECUTABLE=$(which doxygen) \
-  -DSWIG_EXECUTABLE=$(which swig)
+  -DCMAKE_INSTALL_PREFIX="${HOME}/.local/openmm/${OPENMM_VER}" \
+  -DPYTHON_EXECUTABLE="$(which python)" \
+  -DDOXYGEN_EXECUTABLE="$(which doxygen)" \
+  -DSWIG_EXECUTABLE="$(which swig)" \
+  -DPYTHON_INSTALL_PREFIX="${SITE_PACKAGES}" \
+  -DOPENMM_GENERATE_API_DOCS=OFF
 
-make -j$(nproc) install
+make -j"$(nproc)" install
 make PythonInstall
 
-cd ../..
+popd
+
+python -m openmm.testInstallation
