0ca069078b4f211c470cb270c7f1b62b587f6f73
Aketami23 <136186146+Aketami23@users.noreply.github.com>
2025-09-23 21:40:49 +0900

    install_dependencies.sh: refactor installation steps for Doxygen, SWIG, and OpenMM; update requirements.txt to remove pdbfixer

--- experiment_code/install_dependencies.sh
+++ experiment_code/install_dependencies.sh
@@ -1,35 +1,50 @@
 #!/bin/bash
-set -e
+set -eux 
+
+source .venv/bin/activate
+
+pip install --upgrade pip
+pip install Cython setuptools
 
 # 1. Doxygen
-wget -q https://www.doxygen.nl/files/doxygen-1.9.8.linux.bin.tar.gz
-tar -xzf doxygen-1.9.8.linux.bin.tar.gz
-export PATH=$PWD/doxygen-1.9.8/bin:$PATH
+DOXYGEN_VER=1.9.8
+if [ ! -d "doxygen-${DOXYGEN_VER}" ]; then
+  wget -q https://www.doxygen.nl/files/doxygen-${DOXYGEN_VER}.linux.bin.tar.gz
+  tar -xzf doxygen-${DOXYGEN_VER}.linux.bin.tar.gz
+fi
+export PATH=$PWD/doxygen-${DOXYGEN_VER}/bin:$PATH
 
 # 2. SWIG
-wget -q https://github.com/swig/swig/archive/refs/tags/v4.2.1.tar.gz
-tar -xzf v4.2.1.tar.gz
-cd swig-4.2.1
-./autogen.sh
-./configure --prefix=$HOME/apps/swig/4.2.1
-make -j8 && make install
-export PATH=$HOME/apps/swig/4.2.1/bin:$PATH
-cd ..
+SWIG_VER=4.2.1
+if [ ! -x "$HOME/apps/swig/${SWIG_VER}/bin/swig" ]; then
+  wget -q https://github.com/swig/swig/archive/refs/tags/v${SWIG_VER}.tar.gz
+  tar -xzf v${SWIG_VER}.tar.gz
+  cd swig-${SWIG_VER}
+  ./autogen.sh
+  ./configure --prefix=$HOME/apps/swig/${SWIG_VER}
+  make -j$(nproc) && make install
+  cd ..
+fi
+export PATH=$HOME/apps/swig/${SWIG_VER}/bin:$PATH
 
 # 3. OpenMM
-wget -q https://github.com/openmm/openmm/archive/refs/tags/8.1.1.tar.gz
-tar -xzf 8.1.1.tar.gz
-cd openmm-8.1.1
+OPENMM_VER=8.1.1
+if [ ! -d "openmm-${OPENMM_VER}" ]; then
+  wget -q https://github.com/openmm/openmm/archive/refs/tags/${OPENMM_VER}.tar.gz
+  tar -xzf ${OPENMM_VER}.tar.gz
+fi
+
+cd openmm-${OPENMM_VER}
 rm -rf build
 mkdir build && cd build
 
 cmake .. \
-  -DCMAKE_INSTALL_PREFIX=$HOME/apps/openmm/8.1.1 \
+  -DCMAKE_INSTALL_PREFIX=$HOME/apps/openmm/${OPENMM_VER} \
   -DPYTHON_EXECUTABLE=$(which python) \
   -DDOXYGEN_EXECUTABLE=$(which doxygen) \
   -DSWIG_EXECUTABLE=$(which swig)
 
-make -j8 install
+make -j$(nproc) install
 make PythonInstall
 
 cd ../..
