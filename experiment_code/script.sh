69ddc537ad912df474b142587666cd40c3e2e4ec
Aketami23 <136186146+Aketami23@users.noreply.github.com>
2025-09-24 02:31:47 +0900

    dummy.fastaを追加し、script.shのDUMMY_INPUTを更新

--- experiment_code/script.sh
+++ experiment_code/script.sh
@@ -1,6 +1,8 @@
-DUMMY_INPUT="/gs/bs/tga-cddlab/akiba/simulated-annealing_seq_top7/input/20250219/initial-1qys-20250219.fasta"
+DUMMY_INPUT="./data/dummy.fasta"
+mkdir output
 OUTPUT_DIR="output"
 RESULT_CSV="output/results.csv"
+echo "id","parent_ids","generation","negative_tm_score","recovery","negative_plddt_score","raw_jobname","query_sequence", > "$RESULT_CSV"
 YAML_CONFIG="config.yaml"
 python3.10 main.py\
     "$DUMMY_INPUT" \
